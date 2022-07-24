93ef9d07fc030cf9e34af9d72a55066361eb93f9
Dmitriy Kandalov <i@dkandalov.ru>
2022-07-24 11:48:18 +0500

    feat: create a spring project

---
+++ src/main/java/ru/crank/UralSteelAccounting/UralSteelAccountingApplication.java
@@ -0,0 +1,13 @@
+package ru.crank.UralSteelAccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UralSteelAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UralSteelAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/crank/UralSteelAccounting/UralSteelAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.crank.UralSteelAccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UralSteelAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
